d1006f4704751e358ff632eb5e645e7b63c668ba
Jack Spencer <UniformKitten281@gmail.com>
2022-11-13 20:01:38 -0800

    Create include.h

---
+++ src/include.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <windows.h>
+#include <dirent.h>
+#include <errno.h>
+
+#pragma comment(lib, "winmm.lib")
+
+#define MAX_STRING 100
+#define FILE_TYPE ".textcity"
+
+#define PROGRAM_NAME "Bit City"
+#define DOWNLOAD_LINK ""
+#define VERSION "1.0.0"
+#define YEAR 2022
+#define MAINTAINER "Colack"
+#define LICENSE "MIT"
