fd376f22a690087bf336fa72ff4bc4ee062b26f8
Jack Spencer <UniformKitten281@gmail.com>
2022-11-13 20:01:44 -0800

    Create city.h

---
+++ src/city.h
@@ -0,0 +1,15 @@
+#include "include.h"
+
+char player_name[MAX_STRING];
+char player_cityname[MAX_STRING];
+char player_referred[MAX_STRING];
+
+int money;
+int day;
+int homes;
+int factories;
+int stores;
+int workers;
+
+void game();
+void train_start();
