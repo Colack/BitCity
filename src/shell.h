52219cafc25995846f5896c60d33be1b8317f92a
Jack Spencer <UniformKitten281@gmail.com>
2022-11-13 20:01:18 -0800

    Create shell.h

---
+++ src/shell.h
@@ -0,0 +1,12 @@
+#include "city.h"
+
+bool shownStartMessage;
+bool commandFound;
+char consoleInput[MAX_STRING];
+
+void bit_printMain();
+void bit_shell();
+void bit_start();
+void bit_help();
+void bit_version();
+void bit_saves();
